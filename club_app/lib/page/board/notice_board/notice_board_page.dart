0a8b2104d13c4f3b3f8014031bd8f8902c16e41b
jisu <wltnjeon0119@gmail.com>
2021-09-26 17:01:16 +0900

    notice board change

--- club_app/lib/page/board/notice_board/notice_board_page.dart
+++ club_app/lib/page/board/notice_board/notice_board_page.dart
@@ -1,4 +1,6 @@
+//왼쪽정렬이 필요한데 안되네??
 //todo detailpage route null. first, get => navigator error
+import 'package:club_app/components/custsom_text_formfield.dart';
 import 'package:flutter/material.dart';
 //import 'package:club_app/page/board/notice_board/notice_page.dart';
 //import 'package:club_app/page/board/notice_board/create_notice_page.dart';
@@ -15,14 +17,15 @@ class NoticeBoardPage extends StatefulWidget {
 }
 
 class _NoticeBoardPageState extends State<NoticeBoardPage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Container(
-        width: 300,
-        height: double.infinity,
-        color: Colors.grey[300],
-      ),
+    //   drawer: Container(
+    //     width: 300,
+    //     height: double.infinity,
+    //     color: Colors.grey[300],
+    //   ),
       appBar: AppBar(
         backgroundColor: Colors.red[200],
         title: Text('공지사항'),
@@ -57,24 +60,69 @@ class _NoticeBoardPageState extends State<NoticeBoardPage> {
             decoration: BoxDecoration(
               color: Color(0xFFF5CEB8).withOpacity(0.3),
             ),
-          ),ListView.separated(
-          itemCount : 20,
-          itemBuilder: (context, index) {
-            return ListTile(
-              onTap: () { 
-                // Get.to(DetailPage(index),arguments: "arguments 속성 테스트");
-                Navigator.push(context,MaterialPageRoute(builder: (context) {
-                return DetailPage(index);
-                }
+          ),
+          ListView.separated(
+            itemCount : 20,
+            itemBuilder: (context, index) {
+              return ListTile(
+                onTap: () { 
+                  // Get.to(DetailPage(index),arguments: "arguments 속성 테스트");
+                  Navigator.push(context,MaterialPageRoute(builder: (context) {
+                  return DetailPage(index);
+                  }
+                  ),
+                  );
+              } ,
+                title: Transform(
+                  transform: Matrix4.translationValues(-20, 0.0, 0.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Container(width: 190,child: Text('2021.09.26',style: TextStyle(fontSize: 16.0))),
+                      SizedBox(height: 20,),
+                      Center(child: Text('2학기 무야홍 회비 입금 안내',style: TextStyle(fontSize: 23.0,fontWeight: FontWeight.bold),)),
+                      SizedBox(height: 20,),
+                      Text('공지 사항 내용 '*30, style: TextStyle(fontSize: 18),),
+                      Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Column(
+                          children : <Widget>[ 
+                            Divider(height: 40,),
+                            Row(
+                            children: <Widget> [
+                              Image.asset('assets/profile.jpg',
+                                  width: 40, height:40, fit: BoxFit.cover),
+                              Column(
+                                children:<Widget>[
+                                  Padding(padding: const EdgeInsets.only(left: 4.0),
+                                  child: Align(
+                                    alignment: Alignment(-1.0,-1.0),
+                                    child: Row(children :[ SizedBox(width: 5),Text('전지수  | 2 minutes ago',textAlign: TextAlign.start,)]))),
+                                  Padding(padding: const EdgeInsets.all(4.0),
+                                    child:Container(child: Align(
+                                    alignment:Alignment.centerLeft,
+                                    child: Text('언제까지 내야하나요??????????')))
+                                    //왼쪽정렬이 필요한데 안되네??
+                              )])],
+                          ),
+                          CustomTextFormField(hint: "Comment",funValidator: validateContent()),
+                          ]
+                        ),
+                      )
+                      
+                
+                      ]),
                 ),
+                leading: IconButton(
+                  icon: Icon(Icons.check),
+                  onPressed: () {
+                    print("check ");
+                  },
+                ),
+                contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal:0.0),
                 );
-              },
-              title: Text('공지사항 제목',style: TextStyle(fontSize: 20.0),),
-              leading: Text("1"),
-              contentPadding: EdgeInsets.symmetric(vertical: 20.0, horizontal:0.0),
-              );
-            }, 
-             separatorBuilder:  (context, index) {
+              }, 
+            separatorBuilder:  (context, index) {
           return Divider();
              } ,
            ),
