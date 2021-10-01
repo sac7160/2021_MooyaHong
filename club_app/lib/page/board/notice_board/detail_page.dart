98bc6e313f6b045555fe4ef632b55b41e597db92
jisu <wltnjeon0119@gmail.com>
2021-10-01 23:59:29 +0900

    delete null safety

--- club_app/lib/page/board/notice_board/detail_page.dart
+++ club_app/lib/page/board/notice_board/detail_page.dart
@@ -1,4 +1,5 @@
 
+import 'package:club_app/components/custsom_text_formfield.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -13,20 +14,20 @@ class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    String data = Get.arguments;
+    String data = Get.arguments ??"";
 
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.red[300],
+        backgroundColor: Colors.red[200],
         ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Center(child: Text("공지 제목!",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 35),)),
+            Center(child: Text("2학기 무야홍 회비 입금 안내",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),)),
             Divider(),
-            Text("Page Number $id $data;"),
+            Text("Page Number $id $data;2021.09.26 오후 2:16"),
             Divider(),
             Row(
               children: [
@@ -52,10 +53,39 @@ class DetailPage extends StatelessWidget {
                 ),
               ],
             ),
-            Expanded(
+            Container(
               child: SingleChildScrollView(
-                child: Text("공지 내용 " * 500 )))
-          ],),
+                child: Text("공지 사항 내용"*30 ,style: TextStyle(fontSize: 18),))
+                ),
+            Column(children : <Widget>[ 
+                    Divider(height: 40,),
+                    Row(children: <Widget> [
+                      Image.asset('assets/profile.jpg',
+                        width: 40, height:40, fit: BoxFit.cover),
+                          Column(children:<Widget>[
+                            Padding(padding: const EdgeInsets.only(left: 4.0),
+                              child: Align(
+                                alignment: Alignment(-1.0,-1.0),
+                                child: Row(children :[ 
+                                  SizedBox(width: 5),Text('전지수  | 2021.09.26.오후 3:15',textAlign: TextAlign.start,)
+                                  ]
+                                  ))),
+                            Padding(padding: const EdgeInsets.all(4.0),
+                              child:Container(child: Align(
+                                alignment:Alignment.centerLeft,
+                                  child: Text('언제까지 내야하나요??????????')
+                                  )
+                                  )  //왼쪽정렬이 필요한데 안되네??
+                                )
+                                ]
+                                )
+            
+          ],
+          )
+            ],
+      )
+          ]
+        )
       )
     );
   }
